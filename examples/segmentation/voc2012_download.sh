c222d52341f496b2f5ada8cd2791dfa842ec9f67
Davide Cozzolino <davide.cozzolino@unina.it>
2024-05-04 17:53:55 +0000

    pbar download

--- examples/segmentation/voc2012_download.sh
+++ examples/segmentation/voc2012_download.sh
@@ -2,6 +2,6 @@ if [ ! -d voc2012_trainval ]
 then
   wget -nc http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar
   mkdir voc2012_trainval
-  tar -xvf './VOCtrainval_11-May-2012.tar' -C voc2012_trainval
+  tar -xf './VOCtrainval_11-May-2012.tar' -C voc2012_trainval --checkpoint=.1000
 fi
 
