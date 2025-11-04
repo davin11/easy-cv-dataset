72967adb9264a4c55204cab5b5e7cbe84c539b59
Davide Cozzolino <davide.cozzolino@dev102.grip.unina.it>
2025-11-04 16:21:37 +0000

    to_dense

--- easy_cv_dataset/layers/to_tuple.py
+++ easy_cv_dataset/layers/to_tuple.py
@@ -14,6 +14,24 @@
 
 from keras.layers import Layer
 
+def boses_to_dense(bounding_boxes, max_boxes, default_value=-1):
+    try:
+        from tensorflow import RaggedTensor
+    except:
+        return bounding_boxes
+    out_bounding_boxes = dict()
+    for key in bounding_boxes.keys():
+        if isinstance(bounding_boxes[key], RaggedTensor):
+            data_shape = bounding_boxes[key].shape
+            out_bounding_boxes[key] = bounding_boxes[key].to_tensor(
+                default_value=default_value,
+                shape=[data_shape[0], max_boxes] + data_shape[2:],
+            )
+        else:
+            out_bounding_boxes[key] = bounding_boxes[key]
+        
+    return out_bounding_boxes
+
 
 class ToTuple(Layer):
     def __init__(self, dictname_input, dictname_target, max_boxes=None, **kwargs):
@@ -21,11 +39,12 @@ class ToTuple(Layer):
         self.dictname_input = dictname_input
         self.dictname_target = dictname_target
         self.max_boxes = max_boxes
-        assert max_boxes is None
 
     def call(self, dat):
         x = dat[self.dictname_input]
         y = dat[self.dictname_target]
-        #if self.dictname_target == "bounding_boxes":
-        #    y = keras_cv.bounding_box.to_dense(y, max_boxes=self.max_boxes)
+        if (self.dictname_target == "bounding_boxes") and (self.max_boxes is not None):
+            y = boses_to_dense(y, max_boxes=self.max_boxes)
         return x, y
+
+
